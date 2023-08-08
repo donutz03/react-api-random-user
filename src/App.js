290f5b80e77d035178e389f5052828d2ba841413
ionuthodoroaga <ionuthodoroaga@hotmail.com>
2023-08-08 10:36:35 +0300

    feat: added functional counter with button in App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+// https://randomuser.me/api
 
 function App() {
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello codesandbox</h1>
+      <h2>start coding to see sum magik happen</h2>
+      <p>{counter}</p>
+      <button onClick={() =>
+      {
+        setCounter(counter+1)
+      }}>
+        Increase Counter
+        </button>
     </div>
   );
 }
